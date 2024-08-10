2f4171f8778d19548f2ce5aac2a54b26717d01b7
John Gilbey <john@Johns-MacBook-Pro.local>
2024-08-11 00:40:17 +0100

    padded target number and displayed in larger font

--- index.js
+++ index.js
@@ -6,18 +6,18 @@ document.getElementById("calc-button").addEventListener("click", () => {
   const num = randomNum();
   console.log(num); // This will now log the number when the button is clicked
   let strNum = num.toString();
-  let digitOneDiv = document.getElementsByClassName("digit-one")[0];
-  let digitTwiDiv = document.getElementsByClassName("digit-two")[0];
-  let digitThreeDiv = document.getElementsByClassName("digit-three")[0];
-  for (let i = 0; i < strNum; i++) {
-    if (i == 0) {
-      digitOneDiv.innerHTML = strNum[i];
+  // pad with 0s if needed
+  let padding = 3 - strNum.length;
+  if (padding != 0) {
+    if (padding == 1) {
+      strNum = "0" + strNum;
     }
-    if (i == 1) {
-      digitTwiDiv.innerHTML = strNum[i];
-    }
-    if (i == 2) {
-      digitThreeDiv.innerHTML = strNum[i];
+    if (padding == 2) {
+      strNum = "00" + strNum;
     }
   }
+  // update the html display
+  document.getElementsByClassName("digit-one")[0].innerHTML = strNum[0];
+  document.getElementsByClassName("digit-two")[0].innerHTML = strNum[1];
+  document.getElementsByClassName("digit-three")[0].innerHTML = strNum[2];
 });
