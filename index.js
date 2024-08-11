cb2247d2a711391d477e20673657d04e9fb352b8
John Gilbey <john@Johns-MacBook-Pro.local>
2024-08-11 21:04:13 +0100

    genreating number and target. Still to render numbers

--- index.js
+++ index.js
@@ -2,25 +2,25 @@ const randomNum = (min = 0, max = 999) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-// document.getElementById("calc-button").addEventListener("click", () => {
-//   const num = randomNum();
-//   // console.log(num);
-//   let strNum = num.toString();
-//   // pad with 0s if needed
-//   let padding = 3 - strNum.length;
-//   if (padding != 0) {
-//     if (padding == 1) {
-//       strNum = "0" + strNum;
-//     }
-//     if (padding == 2) {
-//       strNum = "00" + strNum;
-//     }
-//   }
-//   // update the html display
-//   document.getElementsByClassName("digit-one")[0].innerHTML = strNum[0];
-//   document.getElementsByClassName("digit-two")[0].innerHTML = strNum[1];
-//   document.getElementsByClassName("digit-three")[0].innerHTML = strNum[2];
-// });
+const target = () => {
+  const num = randomNum();
+  // console.log(num);
+  let strNum = num.toString();
+  // pad with 0s if needed
+  let padding = 3 - strNum.length;
+  if (padding != 0) {
+    if (padding == 1) {
+      strNum = "0" + strNum;
+    }
+    if (padding == 2) {
+      strNum = "00" + strNum;
+    }
+  }
+  // update the html display
+  document.getElementsByClassName("digit-one")[0].innerHTML = strNum[0];
+  document.getElementsByClassName("digit-two")[0].innerHTML = strNum[1];
+  document.getElementsByClassName("digit-three")[0].innerHTML = strNum[2];
+};
 
 const genNum = (topVal, bottomVal) => {
   topArr = [25, 50, 75, 100];
@@ -36,11 +36,27 @@ const genNum = (topVal, bottomVal) => {
     let spliced = bottomArr.splice(idx, 1);
     nums.push(spliced[0]);
   }
+  target();
   return nums;
 };
 
 document.getElementById("top-one").addEventListener("click", () => {
   const nums = genNum(1, 5);
+  console.log(nums);
+});
+
+document.getElementById("top-two").addEventListener("click", () => {
+  const nums = genNum(2, 4);
+  console.log(nums);
+});
+
+document.getElementById("top-three").addEventListener("click", () => {
+  const nums = genNum(3, 3);
+  console.log(nums);
+});
+
+document.getElementById("top-four").addEventListener("click", () => {
+  const nums = genNum(4, 2);
   console.log(nums);
 });
 
