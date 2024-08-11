63f2ad906b6d3b87953175dec328dbd0f5f52c5c
John Gilbey <john@Johns-MacBook-Pro.local>
2024-08-11 16:25:19 +0100

    removed calc-target button added 1/2/3/4 from top buttons

--- index.js
+++ index.js
@@ -2,22 +2,46 @@ const randomNum = (min = 0, max = 999) => {
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-document.getElementById("calc-button").addEventListener("click", () => {
-  const num = randomNum();
-  console.log(num); // This will now log the number when the button is clicked
-  let strNum = num.toString();
-  // pad with 0s if needed
-  let padding = 3 - strNum.length;
-  if (padding != 0) {
-    if (padding == 1) {
-      strNum = "0" + strNum;
-    }
-    if (padding == 2) {
-      strNum = "00" + strNum;
-    }
+// document.getElementById("calc-button").addEventListener("click", () => {
+//   const num = randomNum();
+//   // console.log(num);
+//   let strNum = num.toString();
+//   // pad with 0s if needed
+//   let padding = 3 - strNum.length;
+//   if (padding != 0) {
+//     if (padding == 1) {
+//       strNum = "0" + strNum;
+//     }
+//     if (padding == 2) {
+//       strNum = "00" + strNum;
+//     }
+//   }
+//   // update the html display
+//   document.getElementsByClassName("digit-one")[0].innerHTML = strNum[0];
+//   document.getElementsByClassName("digit-two")[0].innerHTML = strNum[1];
+//   document.getElementsByClassName("digit-three")[0].innerHTML = strNum[2];
+// });
+
+const genNum = (topVal, bottomVal) => {
+  topArr = [25, 50, 75, 100];
+  bottomArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  nums = [];
+  for (i = 0; i < topVal; i++) {
+    let idx = randomNum(0, topArr.length);
+    let spliced = topArr.splice(idx, 1);
+    nums.push(spliced[0]);
   }
-  // update the html display
-  document.getElementsByClassName("digit-one")[0].innerHTML = strNum[0];
-  document.getElementsByClassName("digit-two")[0].innerHTML = strNum[1];
-  document.getElementsByClassName("digit-three")[0].innerHTML = strNum[2];
+  for (i = 0; i < bottomVal; i++) {
+    let idx = randomNum(0, bottomArr.length);
+    let spliced = bottomArr.splice(idx, 1);
+    nums.push(spliced[0]);
+  }
+  return nums;
+};
+
+document.getElementById("top-one").addEventListener("click", () => {
+  const nums = genNum(1, 5);
+  console.log(nums);
 });
+
+// document.getElementsByClassName().addEventListener();
